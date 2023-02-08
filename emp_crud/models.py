5de5aaa1f8e61f0b2e759ce766ba272518e62fd6
Dhananjayan <dhananjayan.online@outlook.com>
2023-02-09 01:45:58 +0530

    forms added urls added models added

--- emp_crud/models.py
+++ emp_crud/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    emp_start_date = models.DateField(auto_now_add=True)
+    emp_end_date = models.DateField()
+    photo = models.ImageField(upload_to='static/images')
+    options = (
+        ('working', 'working'),
+        ('resigned', 'resigned')
+    )
+    status = models.CharField(max_length=100, choices=options, default='working')
